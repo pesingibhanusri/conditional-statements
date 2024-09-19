7658f3f681ad173b8f44c231f4b6fd6226fa24e4
pesingi bhanusri <pesingibhanusri457@gmail.com>
2024-09-19 16:26:32 +0530

    pushing file

---
+++ java task1.js
@@ -0,0 +1,36 @@
+
+
+var num=-6
+ if (num>0){
+    console.log("positive num")
+ }
+ else if(num<0){
+    console.log("negative num")
+ }
+ else {
+    console.log("zero")
+ }
+ if(num%2==0){
+    console.log("even")
+ }
+ else{
+    console.log("odd")
+ }
+  
+
+
+ var a=4
+ var b=5
+
+ if(a,b %2==0){
+    console.log(a+b)
+ }
+else{
+    console.log("not valid")
+}
+
+
+var n=5;
+for(var i=1; i<=10;i++) {
+    console.log(n +"*" +i + "=" +n*i)
+}
